8e86564a8bb777a94af2029cea6942d9adcc575a
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-08-08 17:39:34 +0530

    Update filter logic

--- src/operators.js
+++ src/operators.js
@@ -1,14 +1,14 @@
 const search = {
     '>': (a, b) => (parseFloat(a) > parseFloat(b)),
     '>=': (a, b) => (parseFloat(a) >= parseFloat(b)),
-    '<': (a, b) => (parseFloat(a) < parseFloat(b)),
-    '<=': (a, b) => (parseFloat(a) <= parseFloat(b)),
+    '<': (a, b) => ((parseFloat(a) < parseFloat(b)) && a >= 0),
+    '<=': (a, b) => ((parseFloat(a) <= parseFloat(b)) && a >= 0),
     '!=': (a, b) => (parseFloat(a) != parseFloat(b)),
     '==': (a, b) => (parseFloat(a) == parseFloat(b)),
     'GT': (a, b) => (parseFloat(a) >= parseFloat(b)),
     'LT': (a, b) => (parseFloat(a) <= parseFloat(b)),
     'GTA': (a, b) => (parseFloat(a) >= parseFloat(b)),
-    'LTA': (a, b) => (parseFloat(a) <= parseFloat(b)),
+    'LTA': (a, b) => ((parseFloat(a) <= parseFloat(b)) && a >= 0),
 }
 
 export default search
