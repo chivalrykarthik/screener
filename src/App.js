2a51e61c494968f74dc3afc0d21c0790a1c32adb
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-09-05 17:40:19 +0530

    Added modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Txt from './Txt';
 import Filters from './Filters';
@@ -5,15 +6,22 @@ import Tbl from './Tbl';
 import PercentageDiff from './PercentageDiff';
 import { Container } from './Styles/Container'
 import { Provider, useStore } from './store';
+import Modal from './modal';
 
 function App() {
   const [store] = useStore();
+  const [isModalOpen, setModalOpen] = useState(false);
+  const showModal = () => {
+    setModalOpen(!isModalOpen);
+  }
   return (
     <Container>
       <Txt />
       {store.stocks && store.stocks.length > 0 && <Filters />}
       {store.stocks && store.stocks.length > 0 && <h5>Filters added: {store.filtersCnt}</h5>}
+      {store.compare && store.compare.length > 1 && <button onClick={showModal} >Compare</button>}
       {store.stocks && store.stocks.length > 0 && <Tbl />}
+      {isModalOpen && <Modal showModal={showModal} />}
       <PercentageDiff />
     </Container>
   );
@@ -27,3 +35,12 @@ export default () => {
     </Provider>
   )
 };
+/*import Samp from './ex/sample';
+const App = () => {
+  return (
+    <>
+      <Samp />
+    </>
+  )
+}
+export default App;*/
