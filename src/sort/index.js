fb01f6921a67e06bc2516a8f594b2b1311bc80ad
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-09-19 22:30:08 +0530

    Updated sort logic

--- src/sort/index.js
+++ src/sort/index.js
@@ -4,20 +4,35 @@ import TblView from './../view/TblView';
 
 import { ModalContainer, ModalBody, ModalClose, Content } from './../bestPick/Style';
 
+
+const round5 = (num) => {
+    if (!num) num = 0;
+    let n = num < 0 ? Math.abs(num) : Number(num);
+    const diff = n % 5;
+    const res = diff >= 3 ? n + (5 - diff) : n - diff;
+    const a = num < 0 ? Number(-res) : Number(res);
+    console.log(num + "===" + a)
+    return a;
+}
+
 const sortStocks = (stocks) => {
     return stocks.sort((a, b) => {
-        return b.filters.ROE - a.filters.ROE || b.filters.ROCE - a.filters.ROCE || b.filters.ROEPrevAnn - a.filters.ROEPrevAnn || b.filters.ROCEPrevYr - a.filters.ROCEPrevYr
+        return round5(b.filters.NPMAnn) - round5(a.filters.NPMAnn) || round5(b.filters.NPMPrevAnn) - round5(a.filters.NPMPrevAnn) || round5(b.filters.NPMQtr) - round5(a.filters.NPMQtr) || round5(b.filters.NPMPrevQtr) - round5(a.filters.NPMPrevQtr) || round5(b.filters.NPMPYQtr) - round5(a.filters.NPMPYQtr)
 
-            || b.filters.Salesgrowth - a.filters.Salesgrowth || b.filters.QtrSalesVar - a.filters.QtrSalesVar
+            || round5(b.filters.OPM) - round5(a.filters.OPM) || round5(b.filters.OPMAnn) - round5(a.filters.OPMAnn) || round5(b.filters.OPMPrevAnn) - round5(a.filters.OPMPrevAnn) || round5(b.filters.OPMQtr) - round5(a.filters.OPMQtr) || round5(b.filters.OPMPrevQtr) - round5(a.filters.OPMPrevQtr) || round5(b.filters.OPMPYQtr) - round5(a.filters.OPMPYQtr)
 
 
-            || b.filters.OPM - a.filters.OPM || b.filters.OPMAnn - a.filters.OPMAnn || b.filters.OPMPrevAnn - a.filters.OPMPrevAnn || b.filters.OPMQtr - a.filters.OPMQtr || b.filters.OPMPrevQtr - a.filters.OPMPrevQtr || b.filters.OPMPYQtr - a.filters.OPMPYQtr
+            || round5(b.filters.Salesgrowth) - round5(a.filters.Salesgrowth) || round5(b.filters.QtrSalesVar) - round5(a.filters.QtrSalesVar)
 
-            || b.filters.NPMAnn - a.filters.NPMAnn || b.filters.NPMPrevAnn - a.filters.NPMPrevAnn || b.filters.NPMQtr - a.filters.NPMQtr || b.filters.NPMPrevQtr - a.filters.NPMPrevQtr || b.filters.NPMPYQtr - a.filters.NPMPYQtr
+            || round5(b.filters.EPS12M) - round5(a.filters.EPS12M) || round5(b.filters.EPSAnnRs) - round5(a.filters.EPSAnnRs) || round5(b.filters.EPSPrevAnnRs) - round5(a.filters.EPSPrevAnnRs) || round5(b.filters.EPSQtrRs) - round5(a.filters.EPSQtrRs) || round5(b.filters.EPSPrevQtrRs) - round5(a.filters.EPSPrevQtrRs) || round5(b.filters.EPSPYQtrRs) - round5(a.filters.EPSPYQtrRs)
 
-            || b.filters.EPS12M - a.filters.EPS12M || b.filters.EPSAnnRs - a.filters.EPSAnnRs || b.filters.EPSPrevAnnRs - a.filters.EPSPrevAnnRs || b.filters.EPSQtrRs - a.filters.EPSQtrRs || b.filters.EPSPrevQtrRs - a.filters.EPSPrevQtrRs || b.filters.EPSPYQtrRs - a.filters.EPSPYQtrRs
+            || round5(b.filters.ROE) - round5(a.filters.ROE) || round5(b.filters.ROCE) - round5(a.filters.ROCE) || round5(b.filters.ROEPrevAnn) - round5(a.filters.ROEPrevAnn) || round5(b.filters.ROCEPrevYr) - round5(a.filters.ROCEPrevYr)
 
-            || a.filters['Debt to Eq'] - b.filters['Debt to Eq']
+
+            || round5(a.filters['Debt to Eq']) - round5(b.filters['Debt to Eq'])
+
+
+
     })
 }
 const SortedStocks = () => {
