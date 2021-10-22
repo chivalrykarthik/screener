1e92e529965ae642b5f3b2888072e279ede47119
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-10-22 22:47:37 +0530

    Updated sort cols

--- src/sort/constants.js
+++ src/sort/constants.js
@@ -3,9 +3,11 @@ export const cols = {
     npm: { cols: ['NPMAnn', 'NPMPrevAnn', 'NPMQtr', 'NPMPrevQtr', 'NPMPYQtr'], order: 'desc' },
     opm: { cols: ['OPM', 'OPMAnn', 'OPMPrevAnn', 'OPMQtr', 'OPMPrevQtr', 'OPMPYQtr'], order: 'desc' },
     sales: { cols: ['Salesgrowth', 'QtrSalesVar'], order: 'desc' },
-    roe: { cols: ['ROE', 'ROCE', 'ROEPrevAnn', 'ROCEPrevYr'], order: 'desc' },
+    roe: { cols: ['ROE', 'ROEPrevAnn'], order: 'desc' },
+    roce: { cols: ['ROCE', 'ROCEPrevYr'], order: 'desc' },
     cashFlow: { cols: ['FreeCashFlowRsCr', 'FCFPrevAnnRsCr'], order: 'desc' },
     debt: { cols: ['Debt to Eq'], order: 'asc' },
-    pe: { cols: ['PERatio'], order: 'asc' }
+    pe: { cols: ['PERatio'], order: 'asc' },
+    ey: { cols: ['EarningsYield'], order: 'desc' }
 };
-export const colsOrder = ['eps', 'npm', 'opm', 'sales', 'roe', 'cashFlow', 'debt', 'pe'];
+export const colsOrder = ['eps', 'npm', 'opm', 'sales', 'roe', 'roce', 'cashFlow', 'debt', 'pe', 'ey'];
