5920dc4a4fd0ae04bec583cab57fb0f0da47cc65
Karthik Arumugam <chivalrykarthik@gmail.com>
2022-03-25 22:38:57 +0530

    Updated data

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <div>
           <button onClick={handleChart} >Show Chart</button>
         </div>
-        <div>v2.1.11</div>
+        <div>v2.1.12</div>
       </Header>
       <Container showChart={showChart}>
         {showChart ? <Charts /> : <Screener />}
